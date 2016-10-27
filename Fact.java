f80f17b0a4a8c652721746daa4907a602b1000d3
experion-tibin <tibin.thomas@experionglobal.com>
2016-10-27 13:28:27 +0530

    edited

---
+++ Fact.java
@@ -0,0 +1,17 @@
+﻿ import java.io.*;
+
+public class Fact
+{
+ public static void main(String args[])throws IOException
+	  {
+		int a,s=1;
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter a number\n");
+		a=Integer.parseInt(br.read());
+		for(int i=1;i<=a;i++)
+		s=s*i;
+		System.out.println("Factorial is ",s);
+	}
+}	
+		
+	
